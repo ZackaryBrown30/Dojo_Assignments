9c285a224c6211ff78925424e800c49c015f13ce
ZackaryBrown30 <zackarybrown30@gmail.com>
2020-01-05 21:56:43 -0800

    updating proior to rejoining coursework

--- mean/js_fun/coinchange.js
+++ mean/js_fun/coinchange.js
@@ -1,13 +1,14 @@
-function coinChange (int) {
-    var coinDict = {
-    "dollar": Math.floor(int/100),
-    "quarter": Math.floor((int % 100) /25),
-    "dime" : Math.floor((int % 25) / 10),
-    "nickel" : Math.floor((int % 10) /5),
-    "penny" : Math.floor(int % 5)
-    }
-    console.log(coinDict);
+function coinChange(int) {
+  var coinDict = {
+    dollar: Math.floor(int / 100),
+    quarter: Math.floor((int % 100) / 25),
+    dime: Math.floor((int % 25) / 10),
+    nickel: Math.floor((int % 10) / 5),
+    penny: Math.floor(int % 5)
+  };
+  console.log(coinDict);
 }
 coinChange(543);
 
-//they were put in alphabetical order! whoa
+//they were put in alphabetical order! whats the story there?
+// id love to see examples of this prior to refactoring so efficiently
