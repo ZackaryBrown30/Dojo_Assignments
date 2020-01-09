10042d7e998eedda1e081362fc8bdc00267bb223
ZackaryBrown30 <zackadamianut@gmail.com>
2020-01-08 18:34:20 -0800

    Jan 8 Progresssion through JavaScript es5

---
+++ mean/notes/ninja.js
@@ -0,0 +1,12 @@
+NINJA - 
+
+name;
+health;
+speed(private);
+strength(private);
+
+var s  = "Speed and strength  3 by default && Health 100."
+
+sayName() - {"This should log that Ninja's name to the console"};
+showStats() - {"This should show the Ninja's name, strength, speed, and health"};
+drinkSake() - {"This should add +10 Health to the Ninja"};
